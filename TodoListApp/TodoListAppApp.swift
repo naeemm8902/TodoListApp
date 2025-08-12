513b4506e7c70b55204e83355c147d6b4ccc95ed
Wazir Naeem <113455874+naeemm8902@users.noreply.github.com>
2025-08-12 12:30:02 +0500

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Wazir Naeem Ahmed on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListAppTests/TodoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppTests.swift
+//  TodoListAppTests
+//
+//  Created by Wazir Naeem Ahmed on 12/08/2025.
+//
+
+import Testing
+@testable import TodoListApp
+
+struct TodoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
